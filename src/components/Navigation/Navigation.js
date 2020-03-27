8a836aa6106263584239671608b3abfa815b678d
Cassie <cassielevett@gmail.com>
2020-03-26 20:35:37 -0500

    save scss organization

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import logo from '../../imgs/logo.png'
 
-import './Navigation.css'
-
 class Navigation extends React.Component {
     render() {
         return (
