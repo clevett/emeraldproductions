5705f4834689730c891ce30847a997d09c607f6d
Cassie <cassielevett@gmail.com>
2020-03-26 21:47:20 -0500

    save progress on contact form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import AppCenter  from '../AppCenter/AppCenter'
 import ContactBar from '../ContactBar/ContactBar'
 import SkillsList  from '../SkillsList/SkillsList'
 import ProductionCycle from '../ProductionCycle/ProductionCycle'
+import ContactForm from '../ContactForm/ContactForm'
 
 class App extends React.Component {
   render() {
@@ -16,6 +17,7 @@ class App extends React.Component {
         <ContactBar />
         <SkillsList />
         <ProductionCycle />
+        <ContactForm />
         <footer className='bg-secondary font-italic'>Emerald Productions, LLC 2020</footer>
       </div>
     );
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Col, Row, Container, Form, Button } from 'react-bootstrap'
 
 class ContactForm extends React.Component {
   render() {
     return (
-      <Row>
-        <h1>Contact Me</h1>
-        <p className='mt-3'>Contact me if you have any questions, comments, or feedback.</p>
-      </Row>
+      <section className='bg-primary'>
+        <Container>
+          <Row>
+            <h1>Contact Me</h1>
+          </Row>
+          <Row>
+            <p className='mt-3'>Contact me if you have any questions, comments, or feedback.</p>
+          </Row>
+          <Form>
+            <Form.Row>
+              <Form.Group className='text-left' as={Col} controlId="formGridName">
+                <Form.Label className='text-left'>Name*</Form.Label>
+                <Form.Control type="name" placeholder="Enter name" required />
+              </Form.Group>
+              <Form.Group className='text-left' as={Col} controlId="formGridEmail">
+                <Form.Label>Email*</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" required />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group className='text-left' as={Col} controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Message*</Form.Label>
+                <Form.Control as="textarea" rows="3" />
+              </Form.Group>
+            </Form.Row>
+            <Button variant="dark" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Container>
+      </section>
     )
   }
 }
+
+export default ContactForm
