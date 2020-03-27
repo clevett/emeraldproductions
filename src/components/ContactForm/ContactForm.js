296e5b99314bee372dcabec5186e12479b584837
Cassie <cassielevett@gmail.com>
2020-03-26 21:16:45 -0500

    save contract Form

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+
+class ContactForm extends React.Component {
+  render() {
+    return (
+      <Row>
+        <h1>Contact Me</h1>
+        <p className='mt-3'>Contact me if you have any questions, comments, or feedback.</p>
+      </Row>
+    )
+  }
+}
