d1b766d16869aa3d4c7be2c36b5ac76f038dd6e3
Cassie <cassielevett@gmail.com>
2020-05-23 20:38:52 -0500

    fix carousel

--- src/components/Roll20/Roll20.js
+++ src/components/Roll20/Roll20.js
@@ -3,33 +3,39 @@ import axios from 'axios'
 
 import { Container, Carousel} from 'react-bootstrap'
 import './Roll20.scss'
-import CarouselItem from './CarouselItem/CarouselItem'
 
 class Roll20 extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      sheets: []
+      sheets: [],
     }
   }
 
   componentDidMount() {
     axios.get('http://localhost:5000/Roll20/')
-      .then(response => {
-        this.setState({ sheets: response.data })
-      })
+      .then(response => this.setState({ sheets: response.data }))
       .catch(error => console.log(error))
   }
 
   render() {
     return (
       <Container className='Roll20 text-white'>
-        {
-          this.state.sheets.map(sheet => {
-            const src = `/assets/images/${sheet.image}`
-            return <img src={src} />
-          })
-        }
+        <h1>Character Sheets</h1>
+        <Carousel>
+          {
+            this.state.sheets.map(sheet => {
+              return (
+                <Carousel.Item>
+                  <img
+                    src={`/assets/images/${sheet.image}`}
+                    alt={sheet.name}
+                  />
+                </Carousel.Item>
+              )
+            })
+          }
+        </Carousel>
       </Container>
     )
   }
