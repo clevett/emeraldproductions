d303acd6c64a65bfe90d5c0501880e98126a5a9b
Cassie <cassielevett@gmail.com>
2020-06-13 12:21:53 -0500

    remove unnecessary files

--- server.js
+++ server.js
@@ -34,10 +34,6 @@ app.use('/send', send)
 //ENV
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('./build'))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(_dirname, 'build', 'index.html')) // relative path
-    })
 }
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
