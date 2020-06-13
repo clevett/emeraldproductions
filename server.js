a434875fb8c766516725a32db75e3de3d7e86f15
Cassie <cassielevett@gmail.com>
2020-06-13 11:32:41 -0500

    herok deployable

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require('path')
 
 require('dotenv').config()
 
@@ -11,15 +12,17 @@ app.use(cors())
 app.use(express.json())
 
 const uri = process.env.ATLAS_URI
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri, { 
+    useNewUrlParser: true, useUnifiedTopology: true 
+})
 
 const connection = mongoose.connection
 connection.once('open', () => console.log("MongoDB database connection established successfully"))
 
-const usersRouter = require('./routes/users')
-const sodlbestiaryRouter = require('./routes/sodlbestiary')
-const roll20charsheetsRouter = require('./routes/roll20charsheets')
-const send = require('./routes/send')
+const usersRouter = require('./backend/routes/users')
+const sodlbestiaryRouter = require('./backend/routes/sodlbestiary')
+const roll20charsheetsRouter = require('./backend/routes/roll20charsheets')
+const send = require('./backend/routes/send')
 
 app.use('/users', usersRouter);
 app.use('/ShadowoftheDemonLord', sodlbestiaryRouter)
@@ -28,6 +31,10 @@ app.use('/send', send)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(_dirname, 'build', 'index.html')) // relative path
+    })
 }
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
