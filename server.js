cf2cddab4a20f9eaf62bd7580746399b81f6b8ac
Cassie <cassielevett@gmail.com>
2020-06-13 11:55:22 -0500

    fix server path

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/Roll20', roll20charsheetsRouter)
 app.use('/send', send)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../build'))
+    app.use(express.static('./build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(_dirname, 'build', 'index.html')) // relative path
