dd8de7f34d301dd02b5fe41543f7277ba3c2fa99
Cassie <cassielevett@gmail.com>
2020-06-13 15:08:32 -0500

    change build folder

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/send', send)
 
 //ENV
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./build'))
+    app.use(express.static('build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.join(_dirname, 'build', 'index.html')) // relative path
