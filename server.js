4755d5533f3bd339e48945e2de8d2a3c89b1fc7a
Cassie <cassielevett@gmail.com>
2020-06-13 12:24:57 -0500

    fix the contact form

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ app.use('/send', send)
 //ENV
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('./build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(_dirname, 'build', 'index.html')) // relative path
+    })
 }
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
