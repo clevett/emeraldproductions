008d31882245355216bed13cfec20c420de1256f
Cassie <cassielevett@gmail.com>
2020-06-25 18:34:56 -0500

    add route tests

--- test/route-test.js
+++ test/route-test.js
@@ -3,11 +3,29 @@ const request = require('supertest');
 
 const app = require('../app.js')
 
-describe('root page',  () => {
+describe('/api/ShadowoftheDemonLord',  () => {
   describe('GET request', () => {
       it('returns a 200 status', async () => {
-        const response = await request(app).get('/').send();
+        const response = await request(app).get('/api/ShadowoftheDemonLord').send()
+        assert.equal(response.status, 200)
+      })
+  })
+})
 
+describe('/api/Roll20CharSheets',  () => {
+  describe('GET request', () => {
+      it('returns a 200 status', async () => {
+        const response = await request(app).get('/api/Roll20CharSheets').send()
+        console.log(response)
+        assert.equal(response.status, 200)
+      })
+  })
+})
+
+describe('/api/send',  () => {
+  describe('GET request', () => {
+      it('returns a 200 status', async () => {
+        const response = await request(app).post('/api/send').send()
         assert.equal(response.status, 200)
       })
   })
