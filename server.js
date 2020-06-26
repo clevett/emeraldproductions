fad5fd5dd671cb8e3b10c467732b710b4ebd6892
Cassie <cassielevett@gmail.com>
2020-06-25 20:11:26 -0500

    revert today changes"

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
-const app = require('./app')
+const express = require('express'), cors = require('cors'), mongoose = require('mongoose')
 const path = require('path')
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+//MONGO ATLAS
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, { 
+    useNewUrlParser: true, useUnifiedTopology: true 
+})
+
+const connection = mongoose.connection
+connection.once('open', () => console.log("MongoDB database connection established successfully"))
+
+//ROUTES
+const usersRouter = require('./routes/users')
+const sodlbestiaryRouter = require('./routes/sodlbestiary')
+const roll20charsheetsRouter = require('./routes/roll20charsheets')
+const send = require('./routes/send')
+
+app.use('/users', usersRouter);
+app.use('/api/ShadowoftheDemonLord', sodlbestiaryRouter)
+app.use('/api/Roll20CharSheets', roll20charsheetsRouter)
+app.use('/api/send', send)
 
 //Server static assets in production
 if (process.env.NODE_ENV === 'production') {
