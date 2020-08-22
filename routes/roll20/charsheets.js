acd5312f2fd18d8cecdc7455606baa0d8bccb9dd
Cassie <cassielevett@gmail.com>
2020-08-22 17:22:53 -0500

    fix character sheet route

--- routes/roll20/charsheets.js
+++ routes/roll20/charsheets.js
@@ -3,7 +3,7 @@ const CharacterSheets = require('../../models/roll20/charsheets.model');
 
 router.route('/').get((req, res) => {
   CharacterSheets.find()
-    .then(shadowrun5ejobtype => res.json(shadowrun5ejobtype))
+    .then(roll20charsheets => res.json(roll20charsheets))
     .catch(err => res.status(400).json(`Error: ${err}`));
 });
 
