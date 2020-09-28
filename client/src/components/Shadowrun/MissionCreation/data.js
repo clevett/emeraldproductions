f179505e37352cef449219666359a74ab28f7cbc
Cassie <cassielevett@gmail.com>
2020-09-28 18:53:39 -0500

    fix #17

--- client/src/components/Shadowrun/MissionCreation/data.js
+++ client/src/components/Shadowrun/MissionCreation/data.js
@@ -1,208 +1,208 @@
 const mockdata = [
   {
-    "description": "Secret Society",
+        "description": "Secret Society",
 		"result": [2],
 		"note": "(Black Lodge, Human Nation)",
 		"table": "employer",
-    "source": "Shadowrun 5th Edition"
+        "source": "Shadowrun 5th Edition"
 	},
 	{
-    "description": "Political or Activist Group",
+        "description": "Political or Activist Group",
 		"result": [3],
 		"note": "(Humanis Policlub, Mothers of Metahumans)",
 		"table": "employer",
-    "source": "Shadowrun 5th Edition"
+        "source": "Shadowrun 5th Edition"
 	},
 	{
-    "description": "Government Official or Agency",
+        "description": "Government Official or Agency",
 		"result": [4],
 		"table": "employer",
-    "source": "Shadowrun 5th Edition"
+        "source": "Shadowrun 5th Edition"
 	},
 	{
-    "description": "Minor Corporation",
+        "description": "Minor Corporation",
 		"result": [5, 6],
 		"note": "(A-level or smaller)",
 		"table": "employer",
-    "source": "Shadowrun 5th Edition"
+        "source": "Shadowrun 5th Edition"
 	},
 	{
-    "description": "Megacorporation",
+        "description": "Megacorporation",
 		"result": [7, 8],
 		"note": "(AA-level or larger)",
 		"table": "employer",
-    "source": "Shadowrun 5th Edition"
+        "source": "Shadowrun 5th Edition"
 	},
 	{
-    "description": "Criminal Syndicate",
+        "description": "Criminal Syndicate",
 		"result": [9],
 		"note": "(Yakuza, Mafia)",
 		"table": "employer",
-    "source": "Shadowrun 5th Edition"
+        "source": "Shadowrun 5th Edition"
 	},
 	{
-    "description": "Magical Group",
+        "description": "Magical Group",
 		"result": [10],
 		"note": "(Illuminates of the New Dawn)",
 		"table": "employer",
-    "source": "Shadowrun 5th Edition"
+        "source": "Shadowrun 5th Edition"
 	},
 	{
-    "description": "Private Individual",
+        "description": "Private Individual",
 		"result": [11],
 		"table": "employer",
-    "source": "Shadowrun 5th Edition"
+        "source": "Shadowrun 5th Edition"
 	},
 	{
-    "description": "Exotic or Mysterious Being",
+        "description": "Exotic or Mysterious Being",
 		"result": [12],
 		"note": "(free spirit, dragon, AI)",
 		"table": "employer",
-    "source": "Shadowrun 5th Edition"
+        "source": "Shadowrun 5th Edition"
 	},
 	{
-    "description": "Datasteal",
-    "result": [1],
-    "source": "Shadowrun 5th Edition",
-    "table": "job"
+        "description": "Datasteal",
+        "result": [1],
+        "source": "Shadowrun 5th Edition",
+        "table": "job"
 	},
 	{
-    "description": "Assassination or Destruction",
-    "result": [2],
-    "source": "Shadowrun 5th Edition",
-    "table": "job"
+        "description": "Assassination or Destruction",
+        "result": [2],
+        "source": "Shadowrun 5th Edition",
+        "table": "job"
 	},
 	{
-    "description": "Extraction or Insertion",
-    "result": [3],
-    "source": "Shadowrun 5th Edition",
-    "table": "job"
+        "description": "Extraction or Insertion",
+        "result": [3],
+        "source": "Shadowrun 5th Edition",
+        "table": "job"
 	},
 	{
-    "description": "Misdirection",
-    "result": [4],
-    "source": "Shadowrun 5th Edition",
-    "table": "job"
+        "description": "Misdirection",
+        "result": [4],
+        "source": "Shadowrun 5th Edition",
+        "table": "job"
 	},
 	{
-    "description": "Protection",
-    "result": [5],
-    "source": "Shadowrun 5th Edition",
-    "table": "job"
+        "description": "Protection",
+        "result": [5],
+        "source": "Shadowrun 5th Edition",
+        "table": "job"
 	},
 	{
-    "description": "Delivery",
-    "result": [6],
-    "source": "Shadowrun 5th Edition",
-    "table": "job"
+        "description": "Delivery",
+        "result": [6],
+        "source": "Shadowrun 5th Edition",
+        "table": "job"
 	},
 	{
-    "description": "A key employee",
-    "result": [1],
-    "source": "Shadowrun 5th Edition",
-    "table": "macguffin"
+        "description": "A key employee",
+        "result": [1],
+        "source": "Shadowrun 5th Edition",
+        "table": "macguffin"
 	},
 	{
-    "description": "A prototype product",
-    "result": [2],
-    "source": "Shadowrun 5th Edition",
-    "table": "macguffin"
+        "description": "A prototype product",
+        "result": [2],
+        "source": "Shadowrun 5th Edition",
+        "table": "macguffin"
 	},
 	{
-    "description": "Cutting-edge tech research",
-    "result": [3],
-    "source": "Shadowrun 5th Edition",
-    "table": "macguffin"
+        "description": "Cutting-edge tech research",
+        "result": [3],
+        "source": "Shadowrun 5th Edition",
+        "table": "macguffin"
 	},
 	{
-    "description": "Bioengineered life form",
-    "result": [4],
-    "source": "Shadowrun 5th Edition",
-    "table": "macguffin"
+        "description": "Bioengineered life form",
+        "result": [4],
+        "source": "Shadowrun 5th Edition",
+        "table": "macguffin"
 	},
 	{
-    "description": "Magical object",
-    "result": [5],
-    "source": "Shadowrun 5th Edition",
-    "table": "macguffin"
+        "description": "Magical object",
+        "result": [5],
+        "source": "Shadowrun 5th Edition",
+        "table": "macguffin"
 	},
 	{
-    "description": "Urban building, rural location, or infrastructure object",
-    "result": [6],
-    "source": "Shadowrun 5th Edition",
-    "table": "macguffin"
+        "description": "Urban building, rural location, or infrastructure object",
+        "result": [6],
+        "source": "Shadowrun 5th Edition",
+        "table": "macguffin"
 	},
 	{
-    "description": "Bar, Club, Restaurant",
-    "result": [1],
-    "source": "Shadowrun 5th Edition",
-    "table": "location"
+        "description": "Bar, Club, Restaurant",
+        "result": [1],
+        "source": "Shadowrun 5th Edition",
+        "table": "location"
 	},
 	{
-    "description": "Warehouse, Loading Dock, Factory, or Other Underused Site",
-    "result": [2],
-    "source": "Shadowrun 5th Edition",
-    "table": "location"
+        "description": "Warehouse, Loading Dock, Factory, or Other Underused Site",
+        "result": [2],
+        "source": "Shadowrun 5th Edition",
+        "table": "location"
 	},
 	{
-    "description": "Barrens District or Other Urban Hellhole",
-    "result": [3],
-    "source": "Shadowrun 5th Edition",
-    "table": "location"
+        "description": "Barrens District or Other Urban Hellhole",
+        "result": [3],
+        "source": "Shadowrun 5th Edition",
+        "table": "location"
 	},
 	{
-    "description": "Moving Vehicle",
-    "result": [4],
-    "source": "Shadowrun 5th Edition",
-    "table": "location"
+        "description": "Moving Vehicle",
+        "result": [4],
+        "source": "Shadowrun 5th Edition",
+        "table": "location"
 	},
 	{
-    "description": "Matrix Host",
-    "result": [5],
-    "source": "Shadowrun 5th Edition",
-    "table": "location"
+        "description": "Matrix Host",
+        "result": [5],
+        "source": "Shadowrun 5th Edition",
+        "table": "location"
 	},
 	{
-    "description": "Astral Space",
-    "result": [6],
-    "source": "Shadowrun 5th Edition",
-    "table": "location"
+        "description": "Astral Space",
+        "result": [6],
+        "source": "Shadowrun 5th Edition",
+        "table": "location"
 	},
 	{
-    "description": "Security is unexpectedly high",
-    "result": [1],
-    "source": "Shadowrun 5th Edition",
-    "table": "twist"
+        "description": "Security is unexpectedly high",
+        "result": [1],
+        "source": "Shadowrun 5th Edition",
+        "table": "twist"
 	},
 	{
-    "description": "A third party is also interested",
-    "result": [2],
-    "source": "Shadowrun 5th Edition",
-    "table": "twist"
+        "description": "A third party is also interested",
+        "result": [2],
+        "source": "Shadowrun 5th Edition",
+        "table": "twist"
 	},
 	{
-    "description": "The target is not what it appears to be (group was lied to)",
-    "result": [3],
-    "source": "Shadowrun 5th Edition",
-    "table": "twist"
+        "description": "The target is not what it appears to be (group was lied to)",
+        "result": [3],
+        "source": "Shadowrun 5th Edition",
+        "table": "twist"
 	},
 	{
-    "description": "The job requries a rare piece of equipment",
-    "result": [4],
-    "source": "Shadowrun 5th Edition",
-    "table": "twist"
+        "description": "The job requries a rare piece of equipment",
+        "result": [4],
+        "source": "Shadowrun 5th Edition",
+        "table": "twist"
 	},
 	{
-    "description": "Target has been moved or is being moved",
-    "result": [5],
-    "source": "Shadowrun 5th Edition",
-    "table": "twist"
+        "description": "Target has been moved or is being moved",
+        "result": [5],
+        "source": "Shadowrun 5th Edition",
+        "table": "twist"
 	},
 	{
-    "description": "The employer decides to double-cross the runners",
-    "result": [6],
-    "source": "Shadowrun 5th Edition",
-    "table": "twist"
+        "description": "The employer decides to double-cross the runners",
+        "result": [6],
+        "source": "Shadowrun 5th Edition",
+        "table": "twist"
   }
 ]
 
