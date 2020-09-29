d89e175cf3af72631f0cd6f1cbbe142fc5f1af2b
Cassie Levett <cassielevett@gmail.com>
2020-09-28 22:32:30 -0500

    Travel Tool Weather Die (#19)
    * make weather rollable
    
    * animate the die roll

--- client/src/components/ShadowoftheDemonLord/TravelTool/Weather/Weather.js
+++ client/src/components/ShadowoftheDemonLord/TravelTool/Weather/Weather.js
@@ -1,37 +1,57 @@
 import React from 'react'
+import Roll from 'roll'
 
-import { Row } from 'react-bootstrap'
-import SelectBuilder from '../../../SelectBuilder/SelectBuilder'
+import { Row, Button } from 'react-bootstrap'
 import data from  '../data/weather.js'
 import findObjectByName from '../findObjectByName/findObjectByName'
 
+//Images & Styling
+import './Weather.scss'
+import d6 from '../../../../imgs/icons/perspective-dice-six-faces-six.svg'
 class Weather extends React.Component {
 	constructor(props) {
     super(props)
     this.state = {
-			data,
 			options: data.map(object => object.name) || [],
 			selected: 'Normal conditions',
-			multipier: 1,
+			multiplier: 1,
     }
 	}
 
 	handleChange = async event => {
-		const object = await findObjectByName(data, event)
+		const value = event.target ? event.target.value : event
+		const object = await findObjectByName(data, value)
 
 		this.setState({
 			selected: event,
-			multipier: object.multiplier
+			multiplier: object.multiplier
 		})
 		
-    this.props.onSelectValueChange('weather', this.state.multipier)
+    this.props.onSelectValueChange('weather', this.state.multiplier)
   }
 	
+  handleClick = async event => {
+		event.preventDefault()
+		const roll = new Roll().roll(`3d6`).result
+		const result = data.find(object => object.result.includes(roll))
+
+		this.handleChange(result.name)
+	}
+
+	renderedList = options => options.map(option => <option key={option} option={option} >{option}</option>)
+
 	render() {
 		return (
-			<Row className='justify-content-center mb-2'>
+			<Row className='weather justify-content-center align-items-center'>
 				<h3 className='w-100'>Weather</h3>
-				<SelectBuilder options={this.state.options} selected={this.state.selected} onSelectValueChange={this.handleChange} />
+
+				<select value={this.state.selected} onChange={this.handleChange}>
+					{this.renderedList(this.state.options)}
+        </select>
+
+				<Button onClick={this.handleClick} type="button" variant="link">
+					<img alt='d6' src={d6}></img>
+				</Button>
 			</Row>
 		)
 	}
