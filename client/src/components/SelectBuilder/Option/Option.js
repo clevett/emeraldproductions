8c8eac207059326cd3411ee7cd34a6d20a082864
Cassie <cassielevett@gmail.com>
2020-09-30 23:05:46 -0500

    merge two packages

--- client/src/components/SelectBuilder/Option/Option.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Option = ({ option, selected }) => <option value={option} selected={selected}>{option}</option>
-
-export default Option
--- client/src/components/ShadowoftheDemonLord/TravelTool/determineTravelTime/calculateDays.test.js
+++
@@ -1,8 +0,0 @@
-import { calculateDays } from './determineTravelTime'
-
-describe('Given total hours', () => {
-	describe('When a number is passed', () => {
-		test('Then returns the days truncated', () => expect(calculateDays(10)).toEqual(1))
-		test('Then returns a number', () => expect(typeof calculateDays(30)).toEqual('number'))
-	})
-})
