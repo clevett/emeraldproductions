46151fba5e481429e5dcbed9d008dd59a1695783
Cassie <cassielevett@gmail.com>
2020-10-11 19:31:24 -0500

    add monster name

--- client/src/components/ShadowoftheDemonLord/EncounterBuilder/BeastTable/BeastList/BeastTableRow/BeastTableRow.js
+++ client/src/components/ShadowoftheDemonLord/EncounterBuilder/BeastTable/BeastList/BeastTableRow/BeastTableRow.js
@@ -3,9 +3,9 @@ import React from 'react'
 const BeastTableRow = ({ beastData, beastButton, buttonType }) =>  {
   const buttonDetermination = () => {
     if (buttonType === 'add') {
-      return <button className='btn btn-success' onClick={() => beastButton(beastData, buttonType)}><label className='sr-only'>Add monster</label></button>
+    return <button className='btn btn-success' onClick={() => beastButton(beastData, buttonType)}><label className='sr-only'>Add {beastData.name}</label></button>
     } else {
-      return <button className='btn btn-danger' onClick={() => beastButton(beastData, buttonType)}><label className='sr-only'>Remove monster</label></button>
+      return <button className='btn btn-danger' onClick={() => beastButton(beastData, buttonType)}><label className='sr-only'>Remove {beastData.name}</label></button>
     }
   }
 
