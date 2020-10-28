eed4f762c9a253b011f2a5fe0847ae0a08088870
Cassie <cassielevett@gmail.com>
2020-10-28 15:57:41 -0500

    restyle Karma based on the move

--- client/src/components/Shadowrun/RewardsCalculator/KarmaSwitches/KarmaSwitches.js
+++ client/src/components/Shadowrun/RewardsCalculator/KarmaSwitches/KarmaSwitches.js
@@ -48,19 +48,23 @@ class KarmaSwitches extends React.Component {
 				<Row className='justify-content-center'>
 					<h2>Karma Situation Modifiers</h2>
 				</Row>
-				<Row className='KarmaSwitches switches mb-3'>
-					<Switch key="survived" name="survived" description="Character survived" handleToggle={this.handleToggle} />
-					</Row>
-					<Row className='justify-content-center'>
-						<h3>Group Completed Objectives</h3>
-					</Row>
-					<Row className='slider justify-content-center'>
-						<label className='sr-only' htmlFor="completedObjectives">Choose objectives completed</label>
-						<input type="range" className="custom-range" min="0" max="2" id="completedObjectives" defaultValue='0' onChange={this.sliderChange} />
-						<span>None</span>
-						<span>Some</span>
-						<span>All</span>
-					</Row>
+				<Row>
+					<Col className='switches justify-items-center'>
+						<Switch key="survived" name="survived" description="Character survived" handleToggle={this.handleToggle} />
+					</Col>
+					<Col>
+						<Row className='justify-content-center'>
+							<h3>Group Completed Objectives</h3>
+						</Row>
+						<Row className='slider justify-content-center'>
+							<label className='sr-only' htmlFor="completedObjectives">Choose objectives completed</label>
+							<input type="range" className="custom-range" min="0" max="2" id="completedObjectives" defaultValue='0' onChange={this.sliderChange} />
+							<span>None</span>
+							<span>Some</span>
+							<span>All</span>
+						</Row>
+					</Col>
+				</Row>
 			</Col>
 		)
 	}
