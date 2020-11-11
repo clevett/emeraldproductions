ee13970d1fe394bd48403bc6c51721a7bb2cb5d2
Cassie <cassielevett@gmail.com>
2020-11-10 20:18:28 -0600

    clean up

--- client/src/components/DeckBuilder/DeckBuilder.js
+++ client/src/components/DeckBuilder/DeckBuilder.js
@@ -3,17 +3,15 @@ import React from 'react'
 import Skill from '../Skill/Skill'
 import { CardDeck} from 'react-bootstrap'
 
-class DeckBuilder extends React.Component {
-  render() {
-    const { skills } = this.props
-    return (
-      <CardDeck className=''>
-        {
-        skills.map(skill => <Skill key={skill.name} skill={skill} />)
-        }
-      </CardDeck>
-    )
-  }
+const DeckBuilder = () => {
+  const { skills } = this.props
+  return (
+    <CardDeck className=''>
+      {
+      skills.map(skill => <Skill key={skill.name} skill={skill} />)
+      }
+    </CardDeck>
+  )
 }
 
 export default DeckBuilder
