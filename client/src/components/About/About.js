756b5704a8888c9dff6ae8165fb3c04b2cdf0125
Cassie Levett <cassielevett@gmail.com>
2020-11-10 20:20:05 -0600

    V1.8.3 (#50)
    * minimify css
    
    * fix GA
    
    * trying out router-ga
    
    * save test
    
    * clean up

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -3,18 +3,14 @@ import { Container } from 'react-bootstrap'
 
 import AboutCenter from './AboutCenter/AboutCenter'
 import ContactBar from '../ContactBar/ContactBar'
-//import SkillsList from '../../SkillsList/SkillsList'
-//import ProductionCycle from '../../ProductionCycle/ProductionCycle'
 
-class About extends React.Component {
-  render() {
-    return (
-      <Container className='About text-white'>
-        <AboutCenter />
-        <ContactBar />
-      </Container>
-    )
-  }
+const About = () => {
+  return (
+    <Container className='About text-white'>
+      <AboutCenter />
+      <ContactBar />
+    </Container>
+  )
 }
 
 export default About
