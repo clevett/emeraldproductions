97c95bfa96b866924f56439fa9f02aa2827ff6d5
Cassie <cassielevett@gmail.com>
2020-11-24 13:21:18 -0600

    refactor function component

--- client/src/components/ContactBar/Contact/Contact.js
+++ client/src/components/ContactBar/Contact/Contact.js
@@ -2,22 +2,19 @@ import React from 'react'
 import Image from 'react-bootstrap/Image'
 import { Col, Row } from 'react-bootstrap'
 
-class Contact extends React.Component {
-  render() {
-    const { contact } = this.props
-      return (
-        <Col className='Contact'>
-            <Row>
-              <Col className='m-auto'>
-                <a className='text-white' href={contact.href} target='_blank' rel="noopener noreferrer">
-                  <Image className='shadow mr-2' src={contact.src} alt={`${contact.alt} logo`} />
-                  <span>{contact.name}</span>
-                </a>
-              </Col>
-            </Row>
-        </Col>
-      );     
-  }
+const Contact = ({ contact }) => {
+  return (
+    <Col className='Contact'>
+        <Row>
+          <Col className='m-auto'>
+            <a className='text-white' href={contact.href} target='_blank' rel="noopener noreferrer">
+              <Image className='shadow mr-2' src={contact.src} alt={`${contact.alt} logo`} />
+              <span>{contact.name}</span>
+            </a>
+          </Col>
+        </Row>
+    </Col>
+  )
 }
 
 export default Contact
