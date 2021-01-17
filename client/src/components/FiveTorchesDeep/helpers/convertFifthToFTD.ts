705e8a74aaed53ba4041d0d8fd967a2479d26d1c
Cassie <cassielevett@gmail.com>
2021-01-17 09:55:41 -0600

    working ftd monsters

---
+++ client/src/components/FiveTorchesDeep/helpers/convertFifthToFTD.ts
@@ -0,0 +1,21 @@
+
+const convertFifthToFTD = (fifthEditionMonsters: {}[]):[] => {
+  console.log(fifthEditionMonsters)
+
+  // const monsters = fifthEditionMonsters.map((monster: {}) => {
+  //   const { name, size, challenge_rating } = monster
+  //   return {
+  //     name,
+  //     size,
+  //     hd: challenge_rating
+  //   }
+  // })
+
+  // console.log(monsters)
+
+  const ftdMonsters:any[] = []
+
+  return []
+}
+
+export default convertFifthToFTD
