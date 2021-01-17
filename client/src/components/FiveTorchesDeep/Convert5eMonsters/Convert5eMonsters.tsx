02e8eb3aca42397fb137eb1810a6e72a37477bee
Cassie <cassielevett@gmail.com>
2021-01-17 11:22:39 -0600

    data keeps getting reset to undefined

--- client/src/components/FiveTorchesDeep/Convert5eMonsters/Convert5eMonsters.tsx
+++ client/src/components/FiveTorchesDeep/Convert5eMonsters/Convert5eMonsters.tsx
@@ -3,26 +3,43 @@ import axios from 'axios'
 
 import { Card } from "react-bootstrap"
 
-import SearchBar from '../../SearchBar/SearchBar'
+// import SearchBar from '../../SearchBar/SearchBar'
 
-import fuzzySearch from '../../SearchBar/fuzzySearch/fuzzySearch'
+// import fuzzySearch from '../../SearchBar/fuzzySearch/fuzzySearch'
 
-import convertFifthToFTD from '../helpers/convertFifthToFTD'
+import convertFifthMonsterToFTD from "../helpers/convertFifthMonsterToFTD"
+
+import { MonsterFTD } from "../types/types"
 
 const Convert5eMonsters = () => {
-  const [fifthEditionData, setFifthEditionData] = useState<{}[] | undefined>(undefined)
-  const [monsters] = useState(undefined)
+  const [monsters, setMonsters] = useState<{}[] | undefined>(undefined)
 
   useEffect(() => {
-    getDataFrom5eapi()
+    if(monsters) {
+      console.log(monsters)
+      console.log(monsters[0])
+    }
+  }, [monsters])
+
+  useEffect(() => {
+    getInitialMonstersFrom5eapi(["goblin", "orc", "wolf"])
   }, [])
 
-  useEffect(() => {
-    if(fifthEditionData) {
-      convertFifthToFTD(fifthEditionData)
-    }
-  }, [fifthEditionData])
+  const getInitialMonstersFrom5eapi = async (monsterList:string[]) => {
+    let monsterData: {}[] = []
 
+    await monsterList.forEach(async (monster:string) => {
+      await axios.get(`https://www.dnd5eapi.co/api/monsters/${monster}`)
+      .then((response):void => {
+        monsterData.push(convertFifthMonsterToFTD(response.data))
+      })
+    })
+
+    setMonsters(monsterData)
+  }
+
+  //Get All Monsters but the API cuts them short
+  //This is probably toooooo much on intitial load
   const getDataFrom5eapi = async () => {
     await axios.get('https://www.dnd5eapi.co/api/monsters/')
     .then(async (response) => {
@@ -32,19 +49,40 @@ const Convert5eMonsters = () => {
       await monsterList.forEach(async (monster: { url: string }) => {
         await axios.get(`https://www.dnd5eapi.co${monster.url}`)
         .then((response):void => {
-          monsterData.push(response.data)
+          monsterData.push(convertFifthMonsterToFTD(response.data))
         })
       })
-
-      setFifthEditionData(monsterData)
     })
     .catch(error => console.log(error)) 
+  }
 
-    //this.onTermSubmit('human')
+
+  const monsterCards = (monsterList: any[] | undefined) => {
+    console.log(monsterList)
+    if (monsterList) {
+      monsterList.map((monster: any) => {
+        const { name, size, type, hd } = monster
+  
+        return (
+          <Card style={{ width: '18rem' }}>
+            <Card.Body>
+              <Card.Title>{name}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{size}, {type}</Card.Subtitle>
+              <Card.Text>
+                HD: {hd}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )
+      })
+    } else {
+      return "No monsters found"
+    }
   }
 
   return(
     <div>
+      {monsterCards(monsters)}
     </div>
   )
 }
