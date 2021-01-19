be3c225f2e9d8f447367721b506e3ccbe5f38109
Cassie <cassielevett@gmail.com>
2021-01-18 19:59:26 -0600

    data not loading as it should

--- client/src/components/FiveTorchesDeep/Convert5eMonsters/Convert5eMonsters.tsx
+++ client/src/components/FiveTorchesDeep/Convert5eMonsters/Convert5eMonsters.tsx
@@ -1,90 +1,86 @@
 import React, { useState, useEffect } from "react"
 import axios from 'axios'
 
-import { Card } from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
+import MonsterCards from "../MonsterCards/MonsterCards"
+import { MonsterFTD } from "../types/types"
 
 // import SearchBar from '../../SearchBar/SearchBar'
 
 // import fuzzySearch from '../../SearchBar/fuzzySearch/fuzzySearch'
 
 import convertFifthMonsterToFTD from "../helpers/convertFifthMonsterToFTD"
-
-import { MonsterFTD } from "../types/types"
+import DriveThruLink from '../../DriveThruLink/DriveThruLink'
 
 const Convert5eMonsters = () => {
-  const [monsters, setMonsters] = useState<{}[] | undefined>(undefined)
+  const [monsters, setMonsters] = useState<MonsterFTD[] | undefined>(undefined)
+  const [selectedMonsters, setSelectedMonsters] = useState(["goblin", "orc", "wolf"])
 
   useEffect(() => {
-    if(monsters) {
-      console.log(monsters)
-      console.log(monsters[0])
-    }
+    console.log("USE EFFECT MONSTERS")
+    console.log(monsters)
   }, [monsters])
 
   useEffect(() => {
-    getInitialMonstersFrom5eapi(["goblin", "orc", "wolf"])
+    getInitialMonstersFrom5eapi(selectedMonsters)
   }, [])
 
   const getInitialMonstersFrom5eapi = async (monsterList:string[]) => {
-    let monsterData: {}[] = []
+    // let monsterData: any[] = []
 
-    await monsterList.forEach(async (monster:string) => {
-      await axios.get(`https://www.dnd5eapi.co/api/monsters/${monster}`)
+    await axios.get(`https://www.dnd5eapi.co/api/monsters/goblin`)
       .then((response):void => {
-        monsterData.push(convertFifthMonsterToFTD(response.data))
+        setMonsters([convertFifthMonsterToFTD(response.data)])
       })
-    })
 
-    setMonsters(monsterData)
-  }
+    // await monsterList.forEach(async (monster:string) => {
+    //   await axios.get(`https://www.dnd5eapi.co/api/monsters/${monster}`)
+    //   .then((response):void => {
+    //     monsterData.push(convertFifthMonsterToFTD(response.data))
+    //   })
+    // })
 
-  //Get All Monsters but the API cuts them short
-  //This is probably toooooo much on intitial load
-  const getDataFrom5eapi = async () => {
-    await axios.get('https://www.dnd5eapi.co/api/monsters/')
-    .then(async (response) => {
-      const monsterList = response.data.results
-      let monsterData: {}[] = []
+    // console.log("MONSTER DATA")
+    // console.log(monsterData)
 
-      await monsterList.forEach(async (monster: { url: string }) => {
-        await axios.get(`https://www.dnd5eapi.co${monster.url}`)
-        .then((response):void => {
-          monsterData.push(convertFifthMonsterToFTD(response.data))
-        })
-      })
-    })
-    .catch(error => console.log(error)) 
-  }
-
-
-  const monsterCards = (monsterList: any[] | undefined) => {
-    console.log(monsterList)
-    if (monsterList) {
-      monsterList.map((monster: any) => {
-        const { name, size, type, hd } = monster
-  
-        return (
-          <Card style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>{name}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">{size}, {type}</Card.Subtitle>
-              <Card.Text>
-                HD: {hd}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        )
-      })
-    } else {
-      return "No monsters found"
-    }
+    // if(monsterData.length > 0) {
+    //   setMonsters(monsterData)
+    // } else {
+    //   setMonsters(undefined)
+    // }
   }
 
   return(
-    <div>
-      {monsterCards(monsters)}
-    </div>
+    <Container className='FTDFifthEditionMonsters content text-white'>
+      <Row className='header-row text-center mb-3 justify-content-center w-100'>
+        {/* <img alt='bloody pentagram' src={pentagram}></img> */}
+        <DriveThruLink id='264584' name='Five Torches Deep Fifth Edition Bestiary' />
+        {/* <img alt='bloody pentagram' src={pentagram}></img> */}
+      </Row>
+      <Row>
+        <MonsterCards monsters={monsters} />
+      </Row>
+    </Container>
   )
 }
 
 export default Convert5eMonsters
+
+
+  //Get All Monsters but the API cuts them short
+  //This is probably toooooo much on intitial load
+  // const getDataFrom5eapi = async () => {
+  //   await axios.get('https://www.dnd5eapi.co/api/monsters/')
+  //   .then(async (response) => {
+  //     const monsterList = response.data.results
+  //     let monsterData: {}[] = []
+
+  //     await monsterList.forEach(async (monster: { url: string }) => {
+  //       await axios.get(`https://www.dnd5eapi.co${monster.url}`)
+  //       .then((response):void => {
+  //         monsterData.push(convertFifthMonsterToFTD(response.data))
+  //       })
+  //     })
+  //   })
+  //   .catch(error => console.log(error)) 
+  // }
