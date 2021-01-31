a70eddd493fc87a1f5cbf9fb9be5885765eb0912
Cassie <cassielevett@gmail.com>
2021-01-30 19:25:37 -0600

    keep working on converting monsters

--- client/src/components/FiveTorchesDeep/MonsterCard/MonsterCard.tsx
+++ client/src/components/FiveTorchesDeep/MonsterCard/MonsterCard.tsx
@@ -53,18 +53,27 @@ const MonsterCard = ({ monster }: {monster: MonsterFTD}) => {
           />
         </Card.Title>
         <Card.Subtitle className="mb-2 text-muted">{size}, {type}</Card.Subtitle>
-        <Card.Text>
-          <strong>To Hit: </strong>{displayModifier(modifiers.normal)} <br />
-          <strong>Damage: </strong>{damage} <br /><br />
+        <Card.Text className="mini-cards mini-cards_3">
+          <span>
+            <strong>To Hit: </strong>{displayModifier(modifiers.normal)} <br />
+            <strong>Damage: </strong>{damage} <br /><br />
 
-          <strong>AC: </strong>{ac}<br />
-          <strong>HP: </strong>{hp}<br /><br />
+          </span>
 
-          <strong>Base mod: </strong>{displayModifier(modifiers.normal)} <br />
-          <strong>Speed: </strong>{speed}
+          <span>
+            <strong>AC: </strong>{ac}<br />
+            <strong>HP: </strong>{hp}<br /><br />
+          </span>
+
+          <span>
+            <strong>Base mod: </strong>{displayModifier(modifiers.normal)} <br />
+            <strong>Speed: </strong>{speed}
+          </span>
         </Card.Text>
-        <Description title="Strong" stats={category.strong} modifier={modifiers.strong} />
-        <Description title="Weak" stats={category.weak} modifier={modifiers.weak} />
+        <Card.Text className="mini-cards mini-cards_2">
+          <Description title="Strong" stats={category.strong} modifier={modifiers.strong} />
+          <Description title="Weak" stats={category.weak} modifier={modifiers.weak} />
+        </Card.Text>
         <Abilities abilities={abilities} />
         <Actions actions={actions} />
         <Gold hd={hd} />
--- client/src/components/FiveTorchesDeep/helpers/convertAction/convertAction.ts
+++ client/src/components/FiveTorchesDeep/helpers/convertAction/convertAction.ts
@@ -9,35 +9,47 @@ const removeHumanoidWeapons = (actions:action[]) => {
   return actions.filter(action => !weaponNames.includes(action.name))
 }
 
-const simpleConversions = (hd: number, {name, desc}: action):string => {
-  switch(name) {
-    case "Multiattack":
-      const attacks = Math.floor(hd / 3)
-      return `${attacks} extra attacks`
-    default:
-      return convertDesc(desc)
-  }
+const removeNaturalWeapons = (actions:action[]) => {
+  const weaponNames = ['Bite', 'Claw']
+  return actions.filter(action => !weaponNames.includes(action.name))
 }
 
-const complexConversions = (desc:string):string => {
-  const ifTargetCreature = 'If the target is a creature,'
+const multiAttack = (hd: number) => `${Math.floor(hd / 3)} extra attacks`
 
-  if (desc.includes(ifTargetCreature)) {
-    const splitPhrase = desc.split(ifTargetCreature)[1]
-    return `${ifTargetCreature} ${splitPhrase}`
-  } else {
-    return desc
-  }
+const ifTargetCreature = 'If the target is a creature,'
+
+const forceCondition = (desc:string):string => {
+  const splitPhrase = desc.split(ifTargetCreature)[1]
+  return `${ifTargetCreature} ${splitPhrase}`
+}
+
+const breathToBlast = (hd:number, desc:string):string => {
+  const hdDivideBy2 = Math.max(Math.floor(hd / 2), 1)
+  const hdDivideBy3 = Math.max(Math.floor(hd / 3), 1)
+  const areaSize = hdDivideBy2 > 0 ? hdDivideBy2 * 10 : 10
+  const damage = hdDivideBy2 > 0 ? hdDivideBy2 : 1
+  return`${areaSize}' cone, ${damage}d6 damage, ${hdDivideBy3} user per fight, PCs check to resist or dodge`
 }
 
 const convertAction = (hd:number, actions: action[]):any[] => {
   actions = removeHumanoidWeapons(actions)
+  actions = removeNaturalWeapons(actions)
 
   return actions.map((monsterAction: action) => {
-    const { name } = monsterAction
-    let { desc } = monsterAction
-    desc = simpleConversions(hd, { name, desc })
-    desc = complexConversions(desc)
+    let { name, desc } = monsterAction
+
+    if(name === 'Multi-Attack') {
+      name = "Multi-attack"
+      desc = multiAttack(hd)
+    } else if (desc.includes(ifTargetCreature)) {
+      desc = forceCondition(desc)
+    } else if (name.includes("Breath")) {
+      desc = breathToBlast(hd, desc)
+    }
+    else {
+      desc = convertDesc(desc)
+    }
+
     return { name, desc }
   })
 }
