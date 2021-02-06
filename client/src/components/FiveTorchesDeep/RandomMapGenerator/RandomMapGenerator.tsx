3f1e739ba6396692f5f85a99f9bd9713ce23ff62
Cassie <cassielevett@gmail.com>
2021-02-06 17:23:40 -0600

    add header for ftd

--- client/src/components/FiveTorchesDeep/RandomMapGenerator/RandomMapGenerator.tsx
+++ client/src/components/FiveTorchesDeep/RandomMapGenerator/RandomMapGenerator.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import { Button } from "react-bootstrap"
+import React, { useState } from 'react'
+import { Button, Container, Row } from "react-bootstrap"
 
 import Box from "./Box/Box"
+import DriveThruLink from '../../DriveThruLink/DriveThruLink'
+import logo from "../logo.jpg"
 
 import './RandomMapGenerator.scss'
 
@@ -11,24 +13,31 @@ const RandomMapGenerator = () => {
   const handleClick = () => setRefresh(!refresh)
 
   return (
-    <div className='RandomMapGeneator'>
-      <div key={`map-${refresh}`} className='Map'>
-        <Box name='Northwest'  />
-        <Box name='North'  />
-        <Box name='Northeast'  />
+    <Container className='FTDFifthEditionMonsters content text-white'>
+      <Row className='header-row text-center mb-3 justify-content-center w-100'>
+        <img alt='five with a torch flame' src={logo}></img>
+        <DriveThruLink id='264584' name='Five Torches Deep Fifth Edition Bestiary' />
+        <img alt='five with a torch flame' src={logo}></img>
+      </Row>
+      <div className='RandomMapGeneator'>
+        <div key={`map-${refresh}`} className='Map'>
+          <Box name='Northwest'  />
+          <Box name='North'  />
+          <Box name='Northeast'  />
 
-        <Box name='West'  />
-        <Box name='Center'  />
-        <Box name='East'  />
+          <Box name='West'  />
+          <Box name='Center'  />
+          <Box name='East'  />
 
-        <Box name='Southwest'  />
-        <Box name='South'  />
-        <Box name='Southeast'  />
+          <Box name='Southwest'  />
+          <Box name='South'  />
+          <Box name='Southeast'  />
+        </div>
+        <div className='mt-3'>
+          <Button onClick={handleClick}>Generator New Map</Button>
+        </div>
       </div>
-      <div className='mt-3'>
-        <Button onClick={handleClick}>Generator New Map</Button>
-      </div>
-    </div>
+    </Container>
   )
 }
 
