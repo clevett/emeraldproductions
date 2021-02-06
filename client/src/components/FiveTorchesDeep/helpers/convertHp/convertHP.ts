68312930fd93e052bad311b3a2f96bf287538b7c
Cassie <cassielevett@gmail.com>
2021-02-06 12:13:04 -0600

    add  buttons

--- client/src/components/FiveTorchesDeep/helpers/convertHp/convertHP.ts
+++ client/src/components/FiveTorchesDeep/helpers/convertHp/convertHP.ts
@@ -6,9 +6,13 @@ import challengeCheck from "../challengeCheck/challengeCheck"
 export const rollD8s = ( quanity: number ): number => new Roll().roll(`${quanity}d8`).result
 
 export const convertHP = ( hd: number, hpObject:MonsterMath | undefined ) => {
-  const hpTotal = hpObject ? `${hpObject.hp}` : `${rollD8s(challengeCheck(hd))}`
+  const total = hpObject ? hpObject.hp : rollD8s(challengeCheck(hd))
+  const dice = `${challengeCheck(hd)}d8`
 
-  return `${hpTotal} (${challengeCheck(hd)}d8)`
+  return {
+    total,
+    dice
+  }
 }
 
 export default convertHP
