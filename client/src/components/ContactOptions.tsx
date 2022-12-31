e6b0bf10d4be80810f057e52ec22d5dd757fe1d5
Cassie Levett <cassielevett@gmail.com>
2022-12-31 13:29:31 +0000

    issue-68 (#69)
    * issue-68
    
    * fix issue-66
    
    * change icon issue-70
    
    * save wip
    
    * fix issue-67

--- client/src/components/ContactOptions.tsx
+++ client/src/components/ContactOptions.tsx
@@ -1,5 +1,4 @@
 import { EuiLink, EuiIcon } from "@elastic/eui";
-import discord from "../imgs/Discord-Icon.png";
 import github from "../imgs/GitHub-Icon.png";
 import linkedIn from "../imgs/LinkedIN-Icon.png";
 export const contacts = [
@@ -8,11 +7,6 @@ export const contacts = [
     src: github,
     href: "https://github.com/clevett",
     alt: "silhouette of an octapus cat hybrid",
-  },
-  {
-    name: "Necoya #7621",
-    src: discord,
-    alt: "discord robot eyes in a chat bubble",
   },
   {
     name: "LinkedIN",
