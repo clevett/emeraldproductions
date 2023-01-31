780b11de57415b5c2067575d7f3738214f54eaa2
CassieLevett <cassielevett@gmail.com>
2023-01-31 21:08:56 +0000

    finish heat

--- client/src/components/Shadowrun/Heat.tsx
+++ client/src/components/Shadowrun/Heat.tsx
@@ -3,6 +3,7 @@ import {
   EuiButton,
   EuiLoadingSpinner,
   EuiTitle,
+  EuiTextColor,
 } from "@elastic/eui";
 import React from "react";
 import { useState } from "react";
@@ -26,9 +27,35 @@ export const Heat = () => {
     }
   };
 
-  const modifier = circumstances.map((e) => e.value).reduce((a, b) => a + b, 0);
+  const values = circumstances.map((e) => e.value);
+  const modifier = values.reduce((a, b) => a + b, 0);
   const total = result?.value;
-  console.table({ modifier, total });
+
+  const earned = !total
+    ? null
+    : total && total > 15
+    ? 2
+    : total && total > 9
+    ? 1
+    : 0;
+
+  const message =
+    earned === 2 || earned === 1
+      ? `Runners earn ${earned} heat.`
+      : total
+      ? "Runners earn no heat."
+      : "At the end of every game session.";
+  const color =
+    earned === 2
+      ? "danger"
+      : earned === 1
+      ? "warning"
+      : earned === 0
+      ? "success"
+      : "default";
+
+  const pos = values.filter((n) => n > 0).reduce((a, b) => a + b, 0);
+  const neg = values.filter((n) => n < 0).reduce((a, b) => a + b, 0);
 
   return (
     <LayoutBody
@@ -42,23 +69,18 @@ export const Heat = () => {
             <EuiLoadingSpinner size="s" />
           ) : (
             <div className="grid gap-6 w-full ">
-              <EuiButton
-                aria-label="flip the card"
-                className="self-center min-h-[50px] max-w-[100px] w-6 h-full"
-                onClick={() => roll(`2d6+(${modifier})`)}
-                size="m"
-              >
-                Roll Heat
-              </EuiButton>
               <div className="grid w-full gap-6">
+                <EuiTextColor color={color}>{message}</EuiTextColor>
                 <EuiTitle size="s">
-                  <h4>Positive Circumstances</h4>
+                  <h4>Positive Circumstances{pos ? ` (${pos})` : null}</h4>
                 </EuiTitle>
                 {heat.map((e, index) => (
                   <React.Fragment key={`circumstance-${index}`}>
                     {index === 7 ? (
                       <EuiTitle size="s">
-                        <h4>Negative Circumstances</h4>
+                        <h4>
+                          Negative Circumstances{neg ? ` (${neg})` : null}
+                        </h4>
                       </EuiTitle>
                     ) : null}
                     <Switch
@@ -68,6 +90,14 @@ export const Heat = () => {
                   </React.Fragment>
                 ))}
               </div>
+              <EuiButton
+                aria-label="flip the card"
+                className="self-center min-h-[50px] max-w-[100px] w-6 h-full"
+                onClick={() => roll(`2d6+(${modifier})`)}
+                size="m"
+              >
+                Roll Heat
+              </EuiButton>
             </div>
           )}
           {canvas}
