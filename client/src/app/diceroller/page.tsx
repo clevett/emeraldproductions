eafa28dfd26a62dc2ec7fd027bd339e59b39f951
CassieLevett <cassielevett@gmail.com>
2024-10-15 19:12:07 +0000

    diceroller still broken

--- client/src/app/diceroller/page.tsx
+++ client/src/app/diceroller/page.tsx
@@ -1,9 +1,14 @@
 import { diceroller } from "@/app/resources/metadata";
-import { DiceRoller, Loading } from "@/app/components";
+import { Loading } from "@/app/components";
 import { Suspense } from "react";
 import type { Metadata } from "next";
+import dynamic from "next/dynamic";
 
 export const metadata: Metadata = diceroller;
+
+const DiceRoller = dynamic(() => import("./components/DiceRoller"), {
+  ssr: false,
+});
 
 export default function DiceRollerHome() {
   return (
