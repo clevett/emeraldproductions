44c08feb5ca156355aaf158a0ad14a53f3c2cb76
CassieLevett <cassielevett@gmail.com>
2024-11-28 17:44:49 +0000

    delete the old backups

--- client_backup/src/components/Card/Card.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./styles.module.css";
-export const Card = ({
-  children,
-}: {
-  children: JSX.Element | null | (JSX.Element | null)[];
-}) => {
-  return <div className={`${styles.card}`}>{children}</div>;
-};
--- client_backup/src/helpers/getRandomElement.ts
+++
@@ -1,2 +0,0 @@
-export const getRandomArrayElement = <T>(array: T[]) =>
-  array[Math.floor(Math.random() * array.length)];
