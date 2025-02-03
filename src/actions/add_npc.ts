a48a82e7e45f43a8b60ba27d576a55fab1600d1b
CassieLevett <cassielevett@gmail.com>
2025-02-03 18:07:10 +0000

    save cleanup

--- src/actions/add_npc.ts
+++ src/actions/add_npc.ts
@@ -19,6 +19,4 @@ export const addNpc = (formData: {
       },
     }),
   });
-
-  console.log("AddNpc");
 };
--- src/app/api/shadow_of_the_demon_lord/npcs/route.ts
+++ src/app/api/shadow_of_the_demon_lord/npcs/route.ts
@@ -1,5 +1,4 @@
 import { MongoClient } from "mongodb";
-import { NextRequest } from "next/server";
 
 export async function GET() {
   const client = new MongoClient(process.env.ATLAS_URI ?? "");
@@ -13,31 +12,6 @@ export async function GET() {
     const data = await collection.find({}).toArray();
 
     return Response.json({ data });
-  } catch (error) {
-    Response.json({ message: `Error: ${error}` });
-  } finally {
-    await client.close();
-  }
-}
-
-export async function POST(request: NextRequest) {
-  const client = new MongoClient(process.env.ATLAS_URI_NPCS ?? "");
-  const requestBody = await request.json();
-  const { monster, action } = requestBody;
-
-  console.log("POST", monster);
-  console.log("POST", action);
-
-  try {
-    // await client.connect();
-
-    // const database = client.db(`${process.env.MONGO_DB}`);
-    // const collection = database.collection(`sodlbeasts`);
-    // const data = await collection.insertOne(monster);
-
-    // return Response.json({ data });
-
-    return undefined;
   } catch (error) {
     Response.json({ message: `Error: ${error}` });
   } finally {
